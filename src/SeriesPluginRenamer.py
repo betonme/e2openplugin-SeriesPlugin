222160ead0240e1b3be828c56bca220e87b52206
betonme <glaserfrank@gmail.com>
2013-01-29 20:40:20 +0100

    SeriesPlugin 0.8.6 Identifier memory usage improved Renamer result robustness improved Splitted Wunschliste in Feed and Print version Added caching parameter to license query

--- src/SeriesPluginRenamer.py
+++ src/SeriesPluginRenamer.py
@@ -223,7 +223,7 @@ class SeriesPluginRenameService(object):
 			result = service.getPath()
 		
 		if callable(self.callback):
-			self.callback( str(result) )
+			self.callback( result )
 
 
 #######################################################
@@ -255,7 +255,7 @@ class SeriesPluginRenamer(object):
 
 	def renamerCallback(self, result=None):
 		self.returned += 1
-		if result:
+		if result and isinstance(result, basestring):
 			#Maybe later self.failed.append( name + " " + begin.strftime('%y.%m.%d %H-%M') + " " + channel )
 			self.failed.append( result )
 		
