88458a690bc14a4656fd2baedf5c25a95da9d05d
betonme <glaserfrank@gmail.com>
2014-11-22 13:35:34 +0100

    SeriesPlugin 1.4.8: Fixed typo

--- src/plugin.py
+++ src/plugin.py
@@ -28,7 +28,7 @@ from Logger import splog
 #######################################################
 # Constants
 NAME = "SeriesPlugin"
-VERSION = "1.4.7"
+VERSION = "1.4.8"
 DESCRIPTION = _("SeriesPlugin")
 SHOWINFO = _("Show series info (SP)")
 RENAMESERIES = _("Rename serie(s) (SP)")
