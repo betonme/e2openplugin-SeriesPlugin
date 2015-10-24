0914709994725436ccfc84ea824aba6df223feb5
betonme <glaserfrank@gmail.com>
2015-10-24 19:28:13 +0200

    SeriesPlugin 3.0.5 Removed pickle from cache Added Skinfaktor option to ChannelEditor by zombi

--- src/plugin.py
+++ src/plugin.py
@@ -28,7 +28,7 @@ from Logger import splog
 #######################################################
 # Constants
 NAME = "SeriesPlugin"
-VERSION = "3.0.4"
+VERSION = "3.0.5"
 DESCRIPTION = _("SeriesPlugin")
 SHOWINFO = _("Show series info (SP)")
 RENAMESERIES = _("Rename serie(s) (SP)")
