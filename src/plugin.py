2abb1b416064f2f575674012f51b89d68dde0e02
betonme <glaserfrank@gmail.com>
2015-11-23 18:42:45 +0100

    SeriesPlugin 3.3.3: Identifier: Skip series if name length is smaller or equal to 3

--- src/plugin.py
+++ src/plugin.py
@@ -29,7 +29,7 @@ from Logger import splog
 #######################################################
 # Constants
 NAME = "SeriesPlugin"
-VERSION = "3.3.2"
+VERSION = "3.3.3"
 DESCRIPTION = _("SeriesPlugin")
 SHOWINFO = _("Show series info (SP)")
 RENAMESERIES = _("Rename serie(s) (SP)")
