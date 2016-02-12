718c77ea5eca97097349201a503b10c9ec1f34a9
betonme <glaserfrank@gmail.com>
2016-02-12 18:42:27 +0100

    5.4.0

--- src/plugin.py
+++ src/plugin.py
@@ -30,7 +30,7 @@ from spChannelContextMenu import SPChannelContextMenuInit, SPChannelContextMenuU
 #######################################################
 # Constants
 NAME = "SeriesPlugin"
-VERSION = "5.3.0"
+VERSION = "5.4.0"
 DESCRIPTION = _("SeriesPlugin")
 SHOWINFO = _("Show series info (SP)")
 RENAMESERIES = _("Rename serie(s) (SP)")
