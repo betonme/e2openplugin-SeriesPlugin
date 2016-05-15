43b6c674ee5284065085fe25a86001e2c4f11aab
betonme <glaserfrank@gmail.com>
2016-05-15 11:05:27 +0200

    Centralize get channel name

--- src/SeriesPluginRenamer.py
+++ src/SeriesPluginRenamer.py
@@ -272,7 +272,7 @@ class SeriesPluginRenamer(object):
 						end += (int(config.recording.margin_after.value) * 60)
 					
 					rec_ref_str = info.getInfoString(service, iServiceInformation.sServiceref)
-					#channel = ServiceReference(rec_ref_str).getServiceName()
+					#channel = getChannel( ServiceReference(rec_ref_str) )
 					
 					log.debug("getEpisode:", name, begin, end, rec_ref_str)
 					seriesPlugin.getEpisode(
