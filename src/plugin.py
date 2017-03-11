1f4611bae5a098f13f3bfb021a18365be8049cba
betonme <glaserfrank@gmail.com>
2017-03-11 16:34:48 +0100

    5.9.1: Fixed InfoScreen again

--- src/plugin.py
+++ src/plugin.py
@@ -27,7 +27,7 @@ from spChannelContextMenu import SPChannelContextMenuInit, SPChannelContextMenuU
 #######################################################
 # Constants
 NAME = "SeriesPlugin"
-VERSION = "5.9.0"
+VERSION = "5.9.1"
 DESCRIPTION = _("SeriesPlugin")
 SHOWINFO = _("Show series info (SP)")
 RENAMESERIES = _("Rename serie(s) (SP)")
