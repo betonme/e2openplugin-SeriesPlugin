26ee325972e6c462e6b3022a21e73cce906efaf3
Sven H <svenho@arcor.de>
2018-11-17 11:41:13 +0100

    5.9.5: some fixes
    - fix for use more then one selected webchannel
    - fix for rename description on use 'org' in pattern if episode-title already exist in org-description
    - fix to rename timer (if no EPG-data) with  Option "Check Timer for corresponding EPG events"= no
    - fix for rename-loop of timer-title/description (if no EPG-data) on use 'org' in pattern and option "Add tag 'SeriesPlugin' to timer"= no

--- src/plugin.py
+++ src/plugin.py
@@ -27,7 +27,7 @@ from spChannelContextMenu import SPChannelContextMenuInit, SPChannelContextMenuU
 #######################################################
 # Constants
 NAME = "SeriesPlugin"
-VERSION = "5.9.4"
+VERSION = "5.9.5"
 DESCRIPTION = _("SeriesPlugin")
 SHOWINFO = _("Show series info (SP)")
 RENAMESERIES = _("Rename serie(s) (SP)")
